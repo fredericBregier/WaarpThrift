2904d2179d70e94a19dcb0915a653ecaa5df9f9f
Frederic Bregier <frederic.bregier@waarp.fr>
2019-07-15 12:19:24 +0200

    Fix Pom

--- src/main/java/org/waarp/thrift/util/Version.java
+++ src/main/java/org/waarp/thrift/util/Version.java
@@ -1,10 +1,21 @@
 // DO NOT MODIFY - WILL BE OVERWRITTEN DURING THE BUILD PROCESS
 package org.waarp.thrift.util;
-/** Provides the version information of Waarp Thrift. */
+
+/**
+ * Provides the version information of WaarpThrift.
+ */
 public final class Version {
- /** The version identifier. */
- public static final String ID = "3.0.6";
- /** Prints out the version identifier to stdout. */
- public static void main(String[] args) { System.out.println(ID); }
- private Version() { super(); }
+ /**
+ * The version identifier.
+ */
+ public static final String ID = "3.0.7";
+ /**
+ * Prints out the version identifier to stdout.
+ */
+ public static void main(String[] args) {
+     System.out.println(ID);
+ }
+ private Version() {
+     super();
+ }
 }
